6e3bb4bc1c714e19ba18e225188bff9a47f33faf
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-12-09 12:01:20 +0100

    Refactor frontend code

--- DjangoApp/NEL_project/NEL_app/views.py
+++ DjangoApp/NEL_project/NEL_app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from flair.data import Sentence
 from flair.models import SequenceTagger
-import requests
 
 from .models import *
 from .NER_utils.utils import *
+from .NED_utlis.utils import *
 
 # Load the Flair NER model once (using the 'fast' version)
 print("Loading model...")
@@ -13,37 +13,8 @@ tagger = SequenceTagger.load("flair/ner-english-ontonotes-fast")
 print("Model loaded.")
 
 # Global variables
-DBPEDIA_LOOKUP_ENDPOINT = "https://lookup.dbpedia.org/api/search"
 sentence = None
 
-# DBpedia lookup function
-def search_dbpedia(entity_text, dbpedia_type=None, max_results=3):
-    params = {
-        "query": entity_text,
-        "format": "JSON",
-        "maxResults": max_results,
-    }
-    if dbpedia_type:
-        params["typeName"] = dbpedia_type
-        params["typeNameRequired"] = "true"
-
-    try:
-        response = requests.get(DBPEDIA_LOOKUP_ENDPOINT, params=params)
-        response.raise_for_status()
-        data = response.json()
-
-        if data.get("docs"):
-            best_doc = max(data["docs"], key=lambda doc: float(doc.get("score", [0])[0]))
-            return {
-                "Label": best_doc.get("label", ["Unknown"])[0],
-                "URI": best_doc.get("resource", [""])[0],
-                "Description": best_doc.get("comment", ["No description available"])[0],
-                "Score": float(best_doc.get("score", [0])[0]),
-            }
-    except requests.exceptions.RequestException as e:
-        print(f"Error querying DBpedia: {e}")
-
-    return None
 
 def get_entities_and_links(sentence, text_obj):
     ner_results = []
