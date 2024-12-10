25020cbd1f9546a70fe105a82d377de5ee03dfa5
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-12-10 10:45:03 +0100

    Add wikidata button to frontend

--- DjangoApp/NEL_project/NEL_app/views.py
+++ DjangoApp/NEL_project/NEL_app/views.py
@@ -12,6 +12,7 @@ print("Model loaded.")
 def index(request):
     if request.method == "POST" and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         user_input = request.POST.get("user_input", "")
+        source = request.POST.get("source", "")  # Retrieve the source parameter
 
         if not user_input:
             return JsonResponse({"error": "Input text is required."}, status=400)
@@ -24,8 +25,13 @@ def index(request):
             sentence = Sentence(user_input)
             tagger.predict(sentence, return_probabilities_for_all_classes=True)
 
-            # Pass the text_obj to get_entities_and_links
-            create_entities_in_database(sentence, text_obj)
+            # Determine the source-specific processing
+            if source == "dbpedia":
+                search_entities(sentence, text_obj, knowledge_base="dbpedia")
+            elif source == "wikidata":
+                search_entities(sentence, text_obj, knowledge_base="wikidata")
+            else:
+                return JsonResponse({"error": "Invalid source specified."}, status=400)
 
             # Collect the entities associated with the text
             entities = Entity.objects.filter(text=text_obj).values('entity_text', 'entity_type', 'start_position', 'end_position', 'uri', 'probabilities')
