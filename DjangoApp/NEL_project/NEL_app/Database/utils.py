ef8de9f144e26e62cc9d59de2e9c5ac5604bcf89
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-12-10 10:45:20 +0100

    Add search_wikidata method

--- DjangoApp/NEL_project/NEL_app/Database/utils.py
+++ DjangoApp/NEL_project/NEL_app/Database/utils.py
@@ -1,32 +1,47 @@
-from DjangoApp.NEL_project.NEL_app.NED_utlis.DBpedia.utils import *
+from ..NED_utlis.DBpedia.utils import *
+from ..NED_utlis.Wikidata.utils import *
 from ..models import *
 from ..NER_utils.utils import *
 
 
-def create_entities_in_database(sentence, text_obj):
+def search_entities(sentence, text_obj, knowledge_base):
+    print("Searching in knowledge_base:" + str(knowledge_base))
     ner_results = []
 
+    # Process entities recognized in the sentence
     for entity in sentence.get_spans("ner"):
-        entity_text = entity.text
+
+        entity_label = entity.text
         entity_type = entity.get_label("ner").value
-        best_result = search_dbpedia(entity_text)
+        entity_probabilities = extract_entity_probabilities(entity)
+
+        if knowledge_base == "dbpedia":
+            best_result = search_dbpedia(entity_label)
+        else:
+            best_result = search_wikidata(entity_label)
+
         ner_results.append({
-            "text": entity_text,
+            "label": entity_label,
             "start": entity.start_position,
             "end": entity.end_position,
-            "entity_group": entity_type,
+            "entity_type": entity_type,
             "uri": best_result["URI"] if best_result else "",
+            "probabilities": entity_probabilities
         })
 
-        # Save the entity to the database, associating it with the text_obj
+    # Save all entities in the database
+    save_entities_in_database(ner_results, text_obj)
+
+
+def save_entities_in_database(ner_results, text_obj):
+    for result_entity in ner_results:
         Entity.objects.create(
             text=text_obj,  # Associate with the Text object
-            entity_text=entity_text,
-            entity_type=entity_type,
-            start_position=entity.start_position,
-            end_position=entity.end_position,
-            uri=best_result["URI"] if best_result else "",
-            probabilities=extract_entity_probabilities(entity)  # Get probabilities
+            entity_label=result_entity["label"],
+            entity_type=result_entity["entity_type"],
+            start_position=result_entity["start"],
+            end_position=result_entity["end"],
+            uri=result_entity["uri"],
+            probabilities=result_entity["probabilities"]  # Save probabilities
         )
 
-    return ner_results
