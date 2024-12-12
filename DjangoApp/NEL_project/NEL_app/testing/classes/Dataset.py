16ce7c5743ecada74586189742fd057ea01b2cbd
Krzysztof Nazar <s184698@student.pg.edu.pl>
2024-12-12 17:39:55 +0100

    Add classes instead of Django models

--- DjangoApp/NEL_project/NEL_app/testing/classes/Dataset.py
+++
@@ -1,6 +0,0 @@
-class Dataset:
-    def __init__(self):
-        self.texts = []
-
-    def add_text(self, text):
-        self.texts.append(text)
--- DjangoApp/NEL_project/NEL_app/testing/classes/Text.py
+++
@@ -1,6 +0,0 @@
-class Text:
-    def __init__(self, text, entity_mentions):
-        self.text = text
-        self.entity_mentions = entity_mentions  # A list of EntityMention objects
-
-
