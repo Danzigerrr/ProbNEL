4fe2b22443ae90661076d3c4b406468cd0764d5c
Krzysztof Nazar (remote server) <s184698@student.pg.edu.pl>
2025-04-17 11:04:45 +0200

    Create DataCollectorForCandidateSelectorModel.py

--- DjangoApp/NEL_project/NEL_app/Evaluation/TestText.py
+++
@@ -1,7 +0,0 @@
-from DjangoApp.NEL_project.NEL_app.Models.Text import Text
-
-
-class TestText(Text):
-    def __init__(self, content: str):
-        super().__init__(content)
-        self.logs = []
