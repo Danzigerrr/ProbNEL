9537175cf99325b609a50ba3d88a632d89a2a16f
Krzysztof Nazar (remote server) <s184698@student.pg.edu.pl>
2025-05-06 17:44:39 +0200

    remove the Django project files

--- DjangoApp/NEL_project/NEL_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='NEL_app'),
-    path("run_test_on_dataset/", views.run_test_on_dataset, name="run_test_on_dataset"),
-]
